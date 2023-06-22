8e5d845d80ce493b21dbd1b475a7e3688942645e
aswathcm29 <119151277+aswathcm29@users.noreply.github.com>
2023-06-22 18:28:27 +0530

    Add files via upload
    Added a bit extra responses

--- nova_driveassist.py
+++ nova_driveassist.py
@@ -17,7 +17,7 @@ import random
 
 listener = sr.Recognizer()
 machine = pyttsx3.init()
-openai.api_key = "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"
+openai.api_key = "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"
 
 def talk(text):
     machine.say(text)
@@ -27,7 +27,7 @@ def listen_instruction():
     instruction = "" 
     try:
         with sr.Microphone() as source:
-            print("Listening...")
+            print("lietening")
             audio = listener.listen(source)
             instruction = listener.recognize_google(audio)
             instruction = instruction.lower()
@@ -116,29 +116,42 @@ def get_random_joke():
     return jokes[randint(0, len(jokes)-1)]
 
 def play_nova():
+    global count, is_first_execution
+    if is_first_execution:
+        talk("Hey User, Good to see you back!")
+        print("Hey User, Good to see you back!")
+        talk("I'm Nova, your personal assistant. How can I assist you?")
+        print("I'm Nova, your personal assistant. How can I assist you?")
+        is_first_execution = False
     instruction = listen_instruction()
     print(instruction)
 
     if "play" in instruction:
+        count+=1
         song = instruction.replace('play', '')
-        talk("Playing " + song)
+        talk("Sure, playing " + song)
+        print("Sure, playing " + song)
         pywhatkit.playonyt(song)
 
     elif "time" in instruction:
+        count+=1
         current_time = datetime.datetime.now().strftime('%I:%M %p')
         talk("The current time is " + current_time)
         print("Current time: " + current_time)
 
     elif 'date' in instruction:
+        count+=1
         current_date = datetime.datetime.now().strftime('%d/%m/%Y')
         talk("Today's date is " + current_date)
         print("Current date: " + current_date)
 
     elif 'what is your name' in instruction:
+        count+=1
         talk("I am Nova, your personal assistant. How can I assist you?")
 
 
     elif "weather" in instruction:
+        count+=1
         search_term = instruction.split("for")[-1]
         url = "https://www.google.com/search?sxsrf=ACYBGNSQwMLDByBwdVFIUCbQqya-ET7AAA%3A1578847393212&ei=oUwbXtbXDN-C4-EP-5u82AE&q=weather&oq=weather&gs_l=psy-ab.3..35i39i285i70i256j0i67l4j0i131i67j0i131j0i67l2j0.1630.4591..5475...1.2..2.322.1659.9j5j0j1......0....1..gws-wiz.....10..0i71j35i39j35i362i39._5eSPD47bv8&ved=0ahUKEwiWrJvwwP7mAhVfwTgGHfsNDxsQ4dUDCAs&uact=5"
         webbrowser.get().open(url)
@@ -146,47 +159,56 @@ def play_nova():
 
 
     elif ("tell me a joke" or "joke") in instruction:
+        count+=1
         joke = get_random_joke()
         talk(joke)
         print(joke)
 
     elif 'open google maps' in instruction:
+        count+=1
         talk("Opening Google Maps")
         webbrowser.open("https://www.google.com/maps")    
 
     elif 'search for' in instruction:
+        count+=1
         location = instruction.replace('search for', '').strip()
         search_query = "https://www.google.com/maps/search/" + location.replace(' ', '+')
         talk("Searching for " + location + " on Google Maps")
         webbrowser.open(search_query)
     
     elif 'toss a coin' in instruction:
+        count+=1
         coin = random.choice(['Heads', 'Tails'])
         talk("I tossed a coin and it landed on " + coin)
         print("Coin landed on: " + coin)
 
 
     elif 'who is' in instruction:
+        count+=1
         person = instruction.replace('who is', '')
         info = wikipedia.summary(person, sentences=1)
         print(info)
         talk(info)
 
     elif 'search' in instruction:
+        count+=1
         search_query = instruction.replace('search', '')
         talk("Searching for " + search_query)
         pywhatkit.search(search_query)
 
     elif 'stop' in instruction or 'exit' in instruction:
+        count+=1
         talk("Goodbye!")
         return False
 
     elif 'drowsy' in instruction or 'drowsiness' in instruction:
+        count+=1
         talk("Turning on the drowsiness detection system!")
         print("Turning on the drowsiness detection system!")
         drowsiness_detector()
 
     else:
+        count+=1
         response = generate_chat_response(instruction)
         if response:
             talk(response)
@@ -197,10 +219,12 @@ def play_nova():
 
     return True
 
+count = 0
+is_first_execution = True
+
 while True:
     if not play_nova():
         break
-
 
 
 
